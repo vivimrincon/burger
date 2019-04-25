16de17d6f541daffa88a88fa954930bfd5c08338
ttalgihon <vivithefool@gmail.com>
2019-04-25 16:32:58 -0400

    connecting Jawsdb

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,21 @@ You do so by writing `node -r dotenv/config config/connection.js`
 require('dotenv').config();
 
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-});
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to MySQL Server as id " + connection.threadId);
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: process.env.HOST,
+        port: process.env.PORT,
+        user: process.env.USER,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE
+    });
+    connection.connect(function (err) {
+        if (err) throw err;
+        console.log("Connected to MySQL Server as id " + connection.threadId);
+    });
+};
 
 module.exports = connection; // will be set as module in orm.js
